94a2494488bbec9d38702664f899c3d196b08fce
Austen Mack-Crane <austen@austen-mack-cranes-macbook-pro-7.local>
2020-04-12 14:44:17 -0400

    skeletal tests

---
+++ tests/test_graph.py
@@ -0,0 +1,21 @@
+
+
+import pytest
+
+from ideagraph.graph import IdeaGraph
+
+
+
+
+def test_add_node():
+    assert 2 + 2 == 4
+    pass
+
+
+def test_add_edge():
+    pass
+
+def test_remove_edge():
+    pass
+
+
